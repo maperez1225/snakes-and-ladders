55af321c20301af771cfa2aa4beb3ae9ec3925a5
nicolas gomez <nicolasg1911@gmail.com>
2021-04-26 21:22:15 -0500

    snakes delete

--- src/model/Box.java
+++ src/model/Box.java
@@ -2,8 +2,6 @@ package model;
 
 public class Box {
 	private int id;
-	private Box ladderTo;
-	private Box snakeTo;
 	private Box next;
 	private Box prev;
 	public Box(int i) {
@@ -17,18 +15,6 @@ public class Box {
 	}
 	public void setId(int i) {
 		id = i;
-	}
-	public Box getLadderTo() {
-		return ladderTo;
-	}
-	public void setLadderTo(Box ladderTo) {
-		this.ladderTo = ladderTo;
-	}
-	public Box getSnakeTo() {
-		return snakeTo;
-	}
-	public void setSnakeTo(Box snakeTo) {
-		this.snakeTo = snakeTo;
 	}
 	public Box getNext() {
 		return next;
