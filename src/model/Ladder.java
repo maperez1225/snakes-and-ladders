da64d78a69c58c0d95906cb8009867b0a72038b6
nicolas gomez <nicolasg1911@gmail.com>
2021-04-26 21:32:45 -0500

    ladder filling

--- src/model/Ladder.java
+++ src/model/Ladder.java
@@ -2,8 +2,37 @@ package model;
 
 public class Ladder {
 
+	private int beginning;
+	private int end;
+	private Ladder nextLadder;
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBeginning() {
+		return beginning;
+	}
+	public void setBeginning(int beginning) {
+		this.beginning = beginning;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public Ladder getNextLadder() {
+		return nextLadder;
+	}
+	public void setNextLadder(Ladder nextLadder) {
+		this.nextLadder = nextLadder;
+	}
 	public Ladder() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- src/model/Snake.java
+++ src/model/Snake.java
@@ -2,8 +2,45 @@ package model;
 
 public class Snake {
 
-	public Snake() {
-		// TODO Auto-generated constructor stub
+	private int beginning;
+	private int end;
+	private Snake nextSnake;
+	private char id;
+	
+	public char getId() {
+		return id;
 	}
+	public void setId(char id) {
+		this.id = id;
+	}
+	public Snake() {
+	}
+	public int getBeginning() {
+		return beginning;
+	}
+
+	public void setBeginning(int beginning) {
+		this.beginning = beginning;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	public Snake getNextSnake() {
+		return nextSnake;
+	}
+
+	public void setNextSnake(Snake nextSnake) {
+		this.nextSnake = nextSnake;
+	}
+
+
+
+	
 
 }
