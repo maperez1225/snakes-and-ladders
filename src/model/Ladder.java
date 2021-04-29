2dd1079b8c7056dad0e29c2e957d3667c0cbbace
nicolas gomez <nicolasg1911@gmail.com>
2021-04-28 20:01:04 -0500

    code organization

--- src/model/Ladder.java
+++ src/model/Ladder.java
@@ -5,31 +5,41 @@ public class Ladder {
 	private int end;
 	private Ladder nextLadder;
 	private int id;
+	
 	public Ladder(int i) {
 		id = i;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int i) {
 		id = i;
 	}
+	
 	public int getBeginning() {
 		return beginning;
 	}
+	
 	public void setBeginning(int beginning) {
 		this.beginning = beginning;
 	}
+	
 	public int getEnd() {
 		return end;
 	}
+	
 	public void setEnd(int end) {
 		this.end = end;
 	}
+	
 	public Ladder getNextLadder() {
 		return nextLadder;
 	}
+	
 	public void setNextLadder(Ladder nextLadder) {
 		this.nextLadder = nextLadder;
 	}
+	
 }
