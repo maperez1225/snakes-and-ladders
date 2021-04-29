a5778a7a36b1228fbed9c21e6c4069a196be1b1b
nicolas gomez <nicolasg1911@gmail.com>
2021-04-28 20:01:54 -0500

    code organization 2

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -8,28 +8,37 @@ public class Snake {
 	public Snake(char i) {
 		id = i;
 	}
+	
 	public char getId() {
 		return id;
 	}
+	
 	public void setId(char id) {
 		this.id = id;
 	}
+	
 	public int getBeginning() {
 		return beginning;
 	}
+	
 	public void setBeginning(int beginning) {
 		this.beginning = beginning;
 	}
+	
 	public int getEnd() {
 		return end;
 	}
+	
 	public void setEnd(int end) {
 		this.end = end;
 	}
+	
 	public Snake getNextSnake() {
 		return nextSnake;
 	}
+	
 	public void setNextSnake(Snake nextSnake) {
 		this.nextSnake = nextSnake;
 	}
+	
 }
